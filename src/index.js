68931ba2f3f47fcdd21fe435512a11dc5e961ee0
Maria Bazhenova <yankeevsdixie@gmail.com>
2024-02-22 23:07:53 +0300

    add token, change logic

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
 const doc3 = { id: 'doc3', text: "I'm your shooter." };
 const docs = [doc1, doc2, doc3];
 
+const find = (str, word) => {
+  const prepared = str.split(' ');
+  const term = (token) => `${token.match(/\w+/g)}`;
+  const getTheSame = prepared.filter((item) => term(item) === term(word));
+  return getTheSame;
+};
 const search = (data, query) => {
-    const res = data
-      .filter((item) => item.text.includes(`${query} `))
-      .map((item) => item.id)
-    return res;
-  };
+  const res = data
+    .filter((item) => find(item.text, query).length > 0)
+    .map((item) => item.id)
+  return res;
+};
 
-console.log(search(docs, 'shoot'));
+console.log(search(docs, 'pint!'));
